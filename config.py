e79994954b453324465b227d4c490839be6a1dd8
abdelrhmanmahdY <166949895+abdelrhmanmahdY@users.noreply.github.com>
2025-03-14 16:10:09 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 motor1_en4=21 # move forward
 motor1_en3=20 # move backward
 motor2_en2=16 # move forward
-motor1_en1=12 # move backward  
+motor2_en1=12 # move backward  
+raspberrypi_IP=192.168.137.178 
                                                                              
 
  
